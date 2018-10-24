e89aa02641e5e22983a2ea733cd72978aa258dfa
muirurikin <muirurikin@gmail.com>
2018-10-24 22:36:38 +0300

    Add User Routes

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,25 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello world');
+    res.send('Hello Users');
+});
+router.get('/new', (req, res) => {
+    res.send('Add New User');
+});
+router.post('/', (req, res) => {
+    res.send('User Added');
+});
+router.get('/:id', (req, res) => {
+    res.send('User Profile Page');
+});
+router.get('/:id/edit', (req, res) => {
+    res.send('User Edit Page');
+});
+router.put('/:id', (req, res) => {
+    res.send('User Updated');
+});
+router.delete('/:id', (req, res) => {
+    res.send('User Deleted');
 });
 
 module.exports = router;
