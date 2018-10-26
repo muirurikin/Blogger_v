2c6e470b532e893237116f4e9676ffff454f7123
muirurikin <muirurikin@gmail.com>
2018-10-26 23:05:58 +0300

    Modify register route

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 
 const User = require('../models/user');
@@ -6,20 +7,22 @@ const User = require('../models/user');
 router.get('/login', (req, res) => {
     res.render('users/login');
 });
-router.get('/new', (req, res) => {
+router.get('/register', (req, res) => {
     res.render('users/register');
 });
-router.post('/', (req, res) => {
-    let user = {
+router.post('/register', (req, res) => {
+    let user = new User({
         username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    }
-    User.addUser(user, function(err, user) {
+        email: req.body.email
+    });
+    User.register(user, req.body.password, function(err, user) {
         if (err) {
             console.log(err);
+            res.render('users/register');
         } else {
-            res.json(user);
+            passport.authenticate('local')(req, res, function() {
+                res.redirect('/profile');
+            });
         }
     });
 });
