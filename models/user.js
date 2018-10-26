80688962f4cfa00a031b25b069a0945eaeb1fab5
muirurikin <muirurikin@gmail.com>
2018-10-27 01:25:48 +0300

    Update Files

--- models/user.js
+++ models/user.js
@@ -11,25 +11,21 @@ userSchema.plugin(passportLocalMongoose);
 
 const User = module.exports = mongoose.model('User', userSchema);
 
-module.exports.getUser    = function(id, callback) {
-    User.findById(id, callback);
-};
+// module.exports.getUser    = function(id, callback) {
+//     User.findById(id, callback);
+// };
 
-module.exports.addUser    = function(user, callback) {
-    User.create(user, callback);
-};
+// module.exports.updateUser = function(id, user, options, callback) {
+//     let query = {_id: id};
+//     let update = {
+//         username: user.username,
+//         email: user.email,
+//         password: user.password
+//     };
+//     User.findOneAndUpdate(query, update, options, callback);
+// };
 
-module.exports.updateUser = function(id, user, options, callback) {
-    let query = {_id: id};
-    let update = {
-        username: user.username,
-        email: user.email,
-        password: user.password
-    };
-    User.findOneAndUpdate(query, update, options, callback);
-};
-
-module.exports.removeUser = function(id, callback) {
-    let query = {_id: id};
-    User.findOneAndDelete(query, callback);
-};
+// module.exports.removeUser = function(id, callback) {
+//     let query = {_id: id};
+//     User.findOneAndDelete(query, callback);
+// };
