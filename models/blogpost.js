1bde8e11a53b165bed887479ebabf00cbcdf87a1
muirurikin <muirurikin@gmail.com>
2018-10-29 20:29:37 +0300

    Modify Post Schema

--- models/blogpost.js
+++ models/blogpost.js
@@ -5,6 +5,7 @@ const postSchema = new mongoose.Schema({
     title: { type: String, require:true },
     author: { type: String, require: true},
     body: { type: String, require:true },
+    comments: [ { type: mongoose.Schema.Types.ObjectId, ref: "Comment" } ],
     created: { type: Date, default: Date.now }
 });
 
