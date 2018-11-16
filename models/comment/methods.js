c3facdc83c8ac97b336805c10c6d9e39bbae94af
muirurikin <muirurikin@gmail.com>
2018-11-16 22:42:59 +0300

    Add updateComment Method

--- models/comment/methods.js
+++ models/comment/methods.js
@@ -5,6 +5,16 @@ module.exports = (commentSchema) => {
 };
 
 module.exports = (commentSchema) => {
+  commentSchema.methods.updateComment = async function updateComment(id, comment, options) {
+  const query = { _id: id };
+  const update = {
+    author: comment.author
+  };
+  await this.findOneAndUpdate(query, update, options);
+};
+};
+
+module.exports = (commentSchema) => {
   commentSchema.methods.removeComment = async function removeComment(id) {
     const query = { _id: id };
     await this.findOneAndDelete(query);
